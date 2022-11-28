7ef062cb4050fa7ea38912816bdd36a5e2a00ac8
Alexandru-Gurau <alexandru.gurau@live.com>
2022-11-28 17:46:17 +0200

    adding menu button when mobile screen on

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,24 +1,65 @@
 import './navigation.styles.scss';
 import { Link, Outlet } from 'react-router-dom';
+import hamburgerIcon from '../../assets/icons/hamburger-icon.svg';
+import { useState, useEffect } from 'react';
 
-const Navigation = () => (
-  <>
-    <nav className='navigation'>
-      <Link to='/' className='navigation__link'>
-        Acasa
-      </Link>
-      <Link to='/albums' className='navigation__link'>
-        Albume
-      </Link>
-      <Link to='/about' className='navigation__link'>
-        Despre
-      </Link>
-      <Link to='/contact' className='navigation__link'>
-        Contact me
-      </Link>
-    </nav>
-    <Outlet />
-  </>
-);
+const Navigation = () => {
+  let size;
+  function GetSize() {
+    size = useWindowSize();
+    console.log(size.width < 500 ? true : false);
+    return size;
+  }
+
+  function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      window.addEventListener('resize', handleResize);
+      handleResize();
+
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowSize;
+  }
+
+  GetSize();
+
+  return (
+    <>
+      <nav className='navigation'>
+        {size.width > 500 ? (
+          <>
+            <Link to='/' className='navigation__link'>
+              Acasa
+            </Link>
+            <Link to='/albums' className='navigation__link'>
+              Albume
+            </Link>
+            <Link to='/about' className='navigation__link'>
+              Despre
+            </Link>
+            <Link to='/contact' className='navigation__link'>
+              Contact me
+            </Link>
+          </>
+        ) : (
+          <img alt='Menu button' src={hamburgerIcon} className='menu--button' />
+        )}
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
 export default Navigation;
--- src/routes/contact/contact.page.jsx
+++ src/routes/contact/contact.page.jsx
@@ -2,19 +2,34 @@ import './contact.styles.scss';
 import Header from '../../components/header/header.component';
 import img from '../../assets/images/wallpaper-contact.jpg';
 import whatsappLogo from '../../assets/icons/WhatsAppButtonGreenLarge.svg';
-import messengerLogo from '../../assets/icons/messenger.svg';
+import emailLogo from '../../assets/icons/email.svg';
+// import messengerLogo from '../../assets/icons/messenger.svg'; //doesnt work
 
 const ContactPage = () => {
   return (
     <div className='contact-page'>
-      <Header title='Contact me' img={img} />
+      <Header title='Contactează-mă' img={img} />
       <main className='contact-page__main'>
-        <a aria-label='Chat on WhatsApp' href='https://wa.me/+40747111222'>
-          <img alt='Chat on WhatsApp' src={whatsappLogo} />
-        </a>
-        <a href='http://m.me/alexandru.gurau.96'>
+        <section className='contact-page__program'>
+          <h3 className='h h-large'>Salut, </h3>
+          <p className='p contact-page__text'>
+            Mă poți contacta de luni până vineri între orele 08:00 - 16:00 cu un
+            simplu click pe oricare dintre pictogramele afișate.{' '}
+          </p>
+        </section>
+        <section className='contact-page__messenger'>
+          <a aria-label='Chat on WhatsApp' href='https://wa.me/+40747111222'>
+            <img alt='Chat on WhatsApp' src={whatsappLogo} />
+          </a>
+          <a href='mailto:fotograf@email.com' className='social-link'>
+            <img alt='Send me an email' src={emailLogo} className='icon' /> Send
+            email
+          </a>
+
+          {/* <a href='http://m.me/alexandru.gurau.96'>
           <img alt='Chat on Messenger' src={messengerLogo} className='icon' />
-        </a>
+        </a> */}
+        </section>
       </main>
     </div>
   );
