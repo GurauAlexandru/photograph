f342dbbbdeb08a58c1656f5900743c5357c08130
Alexandru-Gurau <alexandru.gurau@live.com>
2022-12-02 18:02:33 +0200

    fixing navigation bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import './navigation.styles.scss';
-import { useState, useEffect, useContext } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 import { UserContext } from '../../context/user.context';
 import hamburgerIcon from '../../assets/icons/hamburger-icon.svg';
 import closeIcon from '../../assets/icons/close-icon.svg';
@@ -9,38 +9,124 @@ import Icon from '../icon/icon.component';
 import DropdownContainer from '../dropdown-container/dropdown-container.component';
 import DropdownMenu from '../dropdown-menu/dropdown-menu.component';
 
+// const Navigation = () => {
+//   const { isActive, setIsActive } = useContext(UserContext);
+
+//   let size;
+//   function GetSize() {
+//     size = useWindowSize();
+//     return size;
+//   }
+
+//   function useWindowSize() {
+//     const [windowSize, setWindowSize] = useState({
+//       width: undefined,
+//       height: undefined,
+//     });
+
+//     useEffect(() => {
+//       function handleResize() {
+//         setWindowSize({
+//           width: window.innerWidth,
+//           height: window.innerHeight,
+//         });
+//       }
+//       window.addEventListener('resize', handleResize);
+//       handleResize();
+
+//       return () => window.removeEventListener('resize', handleResize);
+//     }, []);
+
+//     return windowSize;
+//   }
+
+//   GetSize();
+
+//   const turnOn = () => {
+//     setIsActive(true);
+//   };
+
+//   const turnOff = () => {
+//     setIsActive(false);
+//   };
+
+//   return (
+//     <>
+//       <nav className='navigation'>
+//         {size.width > 500 ? (
+//           <>
+//             <Link to='/' className='navigation__link'>
+//               Acasa
+//             </Link>
+//             <Link to='/albums' className='navigation__link'>
+//               Albume
+//             </Link>
+//             <Link to='/about' className='navigation__link'>
+//               Despre
+//             </Link>
+//             <Link to='/contact' className='navigation__link'>
+//               Contact me
+//             </Link>
+//           </>
+//         ) : (
+//           <Button className='button--menu' onClick={() => turnOn()}>
+//             <Icon src={hamburgerIcon} alt='Menu button' className='icon-btn' />
+//           </Button>
+//         )}
+//       </nav>
+//       <Outlet />
+//       {isActive ? (
+//         <DropdownContainer>
+//           {
+//             <>
+//               <Button
+//                 className='button--menu btn--right'
+//                 onClick={() => turnOff()}
+//               >
+//                 <Icon alt='Close modal' src={closeIcon} className='icon-btn' />
+//               </Button>
+//               <DropdownMenu>
+//                 <Link
+//                   to='/'
+//                   className='navigation__link navigation__link--mobile'
+//                   onClick={() => turnOff()}
+//                 >
+//                   Acasa
+//                 </Link>
+//                 <Link
+//                   to='/albums'
+//                   className='navigation__link navigation__link--mobile'
+//                   onClick={() => turnOff()}
+//                 >
+//                   Albume
+//                 </Link>
+//                 <Link
+//                   to='/about'
+//                   className='navigation__link navigation__link--mobile'
+//                   onClick={() => turnOff()}
+//                 >
+//                   Despre
+//                 </Link>
+//                 <Link
+//                   to='/contact'
+//                   className='navigation__link navigation__link--mobile'
+//                   onClick={() => turnOff()}
+//                 >
+//                   Contact me
+//                 </Link>
+//               </DropdownMenu>
+//             </>
+//           }
+//         </DropdownContainer>
+//       ) : (
+//         ''
+//       )}
+//     </>
+//   );
+// };
+
 const Navigation = () => {
   const { isActive, setIsActive } = useContext(UserContext);
-
-  let size;
-  function GetSize() {
-    size = useWindowSize();
-    return size;
-  }
-
-  function useWindowSize() {
-    const [windowSize, setWindowSize] = useState({
-      width: undefined,
-      height: undefined,
-    });
-
-    useEffect(() => {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-      window.addEventListener('resize', handleResize);
-      handleResize();
-
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowSize;
-  }
-
-  GetSize();
 
   const turnOn = () => {
     setIsActive(true);
@@ -50,31 +136,32 @@ const Navigation = () => {
     setIsActive(false);
   };
 
+  console.log(isActive);
+
   return (
     <>
       <nav className='navigation'>
-        {size.width > 500 ? (
-          <>
-            <Link to='/' className='navigation__link'>
-              Acasa
-            </Link>
-            <Link to='/albums' className='navigation__link'>
-              Albume
-            </Link>
-            <Link to='/about' className='navigation__link'>
-              Despre
-            </Link>
-            <Link to='/contact' className='navigation__link'>
-              Contact me
-            </Link>
-          </>
-        ) : (
-          <Button className='button--menu' onClick={() => turnOn()}>
-            <Icon src={hamburgerIcon} alt='Menu button' className='icon-btn' />
-          </Button>
-        )}
+        <Link to='/' className='navigation__link'>
+          Acasa
+        </Link>
+        <Link to='/albums' className='navigation__link'>
+          Albume
+        </Link>
+        <Link to='/about' className='navigation__link'>
+          Despre
+        </Link>
+        <Link to='/contact' className='navigation__link'>
+          Contact me
+        </Link>
+
+        <Button
+          className='button--menu button--mobile'
+          onClick={() => turnOn()}
+        >
+          <Icon src={hamburgerIcon} alt='Menu button' className='icon-btn' />
+        </Button>
       </nav>
-      <Outlet />
+
       {isActive ? (
         <DropdownContainer>
           {
