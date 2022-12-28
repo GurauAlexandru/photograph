e322dd6d365b839b5c940c9101181552d079d660
Alexandru-Gurau <alexandru.gurau@live.com>
2022-12-28 21:26:35 +0200

    changing header text with handwrite

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ const Header = ({ img, alt, title, description, children }) => (
   <header className='header'>
     <section className='header__box'>
       <h1 className='h h--xLarge'>{title}</h1>
-      <h3 className='h h--small'>{description}</h3>
+      <h3 className='h h--small handwrite'>{description}</h3>
       {children}
     </section>
     <section className='header__box'>
