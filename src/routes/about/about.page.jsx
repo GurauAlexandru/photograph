9a5771669aad35f6d12ff8e45da285aae2620131
Alexandru-Gurau <alexandru.gurau@live.com>
2023-01-04 11:51:16 +0200

    finish about page and changing home page button

--- src/routes/about/about.page.jsx
+++ src/routes/about/about.page.jsx
@@ -1,18 +1,48 @@
 import './about.styles.scss';
 import Navigation from '../../components/navigation/navigation.component';
-import Header from '../../components/header/header.component';
-import img from '../../assets/images/wallpaper-despre-c.jpg';
+import signagureLogo from '../../assets/icons/logo--black.png';
+import fotografFoto from '../../assets/images/wallpaper-photograph.jpg';
 
 const AboutPage = () => {
   return (
     <>
       <Navigation />
-      <Header
-        img={img}
-        alt='image alt'
-        title='About page'
-        description='Welcome to about page'
-      />
+      <section className='about-page'>
+        <main className='about-page__container'>
+          <h1 className='h h--mega'>Florin</h1>
+          <h1 className='h h--mega'>Gireadă</h1>
+          <article>
+            <p className='paragraph'>
+              {' '}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eos
+              accusantium dolore ratione mollitia quo. Accusamus nemo excepturi
+              fuga aliquam ab pariatur. Maxime, molestias! Fugit iusto sit aut
+              nobis quasi eligendi nihil cum consequatur alias!
+            </p>
+          </article>
+          <article>
+            <p className='paragraph'>
+              {' '}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eos
+              accusantium dolore ratione mollitia quo. Accusamus nemo excepturi
+              fuga aliquam ab pariatur. Maxime, molestias! Fugit iusto sit aut
+              nobis quasi eligendi nihil cum consequatur alias!
+            </p>
+          </article>
+          <img
+            src={signagureLogo}
+            alt='semnătura mea'
+            className='about-page__logo'
+          />
+        </main>
+        <div className='about-page__box'>
+          <img
+            src={fotografFoto}
+            alt='poza fotografului'
+            className='about-page__foto'
+          />
+        </div>
+      </section>
     </>
   );
 };
