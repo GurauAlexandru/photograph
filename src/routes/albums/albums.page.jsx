0936b0a69111e09fe5ee4ae5ce8c03c161f9e1f5
Alexandru-Gurau <alexandru.gurau@live.com>
2023-01-09 19:58:16 +0200

    albums page: adding header

--- src/routes/albums/albums.page.jsx
+++ src/routes/albums/albums.page.jsx
@@ -1,27 +1,36 @@
 import './albums.styles.scss';
 import Navigation from '../../components/navigation/navigation.component';
 import Footer from '../../components/footer/footer.component';
-// import nuntiImg from '../../assets/images/wallpaper-albums-c.jpg';
+import albumWallpaper from '../../assets/poze/wallpaper--albums.webp';
+import brideWallaper from '../../assets/poze/wallpaper--bride.webp';
 
 const AlbumsPage = () => {
   return (
     <>
       <Navigation />
-      {/* <main className='album-page'>
+      <main className='album-page'>
         <section className='album-preview'>
           <div className='album-preview__container'>
-            <h3 className='h h--big'>Nunti</h3>
+            <h1 className='h h--xLarge'>Albume foto</h1>
+            <p className='paragraph'>
+              Mai jos vei găsi linkurile catre albumele de nunți, botezuri, poze
+              de produs, de familie, etc.
+            </p>
           </div>
+          <img
+            src={brideWallaper}
+            alt='album wallpaper 2'
+            className='album-page__wallpaper'
+          />
           <div className='album-preview__box'>
             <img
-              className='album-preview__image'
-              alt='poza nunta'
-              src={nuntiImg}
+              src={albumWallpaper}
+              alt='album wallpaper'
+              className='album-preview__wallpaper'
             />
           </div>
         </section>
-
-      </main> */}
+      </main>
       <Footer />
     </>
   );
